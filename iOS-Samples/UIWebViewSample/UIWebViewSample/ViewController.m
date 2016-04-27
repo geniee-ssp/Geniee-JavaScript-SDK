0df66ebb772984a7bc2d998f371d50112b22fdf7
gntakahashi <n.takahashi@geniee.co.jp>
2016-04-27 18:18:27 +0900

    remove redundant import and fix desc

--- iOS-Samples/UIWebViewSample/UIWebViewSample/ViewController.m
+++ iOS-Samples/UIWebViewSample/UIWebViewSample/ViewController.m
@@ -45,6 +45,7 @@
 
     // URLに外部ブラウザで起動するためのキーワードが含まれていたら外部ブラウザで開く処理
     // (広告に管理画面で設定したキーワードが設定され、広告をタップすると外部ブラウザで開く処理を実現する)
+    // 下の例の、@"外部ブラウザ起動用キーワード"を登録済みのものに書き換える。
     if ([urlString rangeOfString:@"外部ブラウザ起動用キーワード"].location != NSNotFound) {
         [[UIApplication sharedApplication] openURL:url];
         return NO;
