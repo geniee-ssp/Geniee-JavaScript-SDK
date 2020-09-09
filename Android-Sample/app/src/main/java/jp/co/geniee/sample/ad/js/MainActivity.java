de920ebd72f9fb2cb87ad803e54adbd91fdfe4e8
kajimac1 <kajimac1>
2020-09-09 20:00:33 +0900

    API30対応 wip

--- Android-Sample/app/src/main/java/jp/co/geniee/sample/ad/js/MainActivity.java
+++ Android-Sample/app/src/main/java/jp/co/geniee/sample/ad/js/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
